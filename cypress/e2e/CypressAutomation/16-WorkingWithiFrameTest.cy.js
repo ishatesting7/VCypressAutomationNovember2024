645087be2213e76d7b262c16034a63167eb48bd1
harafit <harafittesting@gmail.com>
2024-12-01 19:41:17 +0530

    Added iFrame:

--- cypress/e2e/CypressAutomation/16-WorkingWithiFrameTest.cy.js
+++ cypress/e2e/CypressAutomation/16-WorkingWithiFrameTest.cy.js
@@ -6,7 +6,6 @@ describe('Iframe Demo', ()=>{
 
         cy.frameLoaded('[title="W3Schools HTML Tutorial"]');
 
-        
         cy.iframe('[title="W3Schools HTML Tutorial"]').wait(2000).contains('CSS').click();
     })
 })
